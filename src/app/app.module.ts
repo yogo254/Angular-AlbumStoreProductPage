59f2f99cd21b124bed58cfb47a5f278c29eda70d
yogo254 <yogo254@yahoo.com>
2020-09-30 19:36:17 +0300

    part 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from "./app.component";
 import { ProductPageComponent } from "./product-page/product-page.component";
 import { ProductDescriptionComponent } from "./product-description/product-description.component";
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
     ProductPageComponent,
     ProductDescriptionComponent,
     ProductTracklistingComponent,
+    ProductListComponent,
   ],
   imports: [BrowserModule, FormsModule, HttpModule],
   providers: [ProductService],
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,10 +3,12 @@ import { Http, Response } from "@angular/http";
 import { Album } from "./album";
 import "rxjs/add/operator/map";
 import { Observable } from "rxjs/Observable";
+import { Product } from "./product";
 
 @Injectable()
 export class ProductService {
   private _albumUrl: string = "../assets/album.json";
+  private _productsUrl: string = "../assets/products.json";
 
   constructor(private _http: Http) {}
 
@@ -14,5 +16,10 @@ export class ProductService {
     return this._http
       .get(this._albumUrl)
       .map((response) => <Album>response.json());
+  }
+  getProducts(): Observable<Product[]> {
+    return this._http
+      .get(this._productsUrl)
+      .map((response) => <Product[]>response.json());
   }
 }
