c8a03b7026adfcc6712a71b9522840afae29767d
yogo254 <yogo254@yahoo.com>
2020-09-30 19:51:57 +0300

    part 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,29 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { HttpModule } from "@angular/http";
 import { ProductService } from "./product.service";
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { ProductPageComponent } from "./product-page/product-page.component";
 import { ProductDescriptionComponent } from "./product-description/product-description.component";
-import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ProductTracklistingComponent } from "./product-tracklisting/product-tracklisting.component";
+import { ProductListComponent } from "./product-list/product-list.component";
+
+const appRoutes: Routes = [
+  {
+    path: "products",
+    component: ProductListComponent,
+  },
+  {
+    path: "product/:id",
+    component: ProductPageComponent,
+  },
+  {
+    path: "",
+    redirectTo: "products",
+    pathMatch: "full",
+  },
+];
 
 @NgModule({
   declarations: [
@@ -18,7 +35,12 @@ import { ProductListComponent } from './product-list/product-list.component';
     ProductTracklistingComponent,
     ProductListComponent,
   ],
-  imports: [BrowserModule, FormsModule, HttpModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+  ],
   providers: [ProductService],
 
   bootstrap: [AppComponent],
